87b37b9170f035cfe381992966728f2188fd4ac8
ff <ffungproyectos@gmail.com>
2020-02-28 00:49:16 -0400

    Fix issue

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@
 * 4. Applied InfoSec Challenges
 * =============================
 ***********************************************/
-
+const helmet = require('helmet');
 var express = require('express'); // Do Not Edit
 var app = express();              // Do Not Edit
 
@@ -16,8 +16,6 @@ var app = express();              // Do Not Edit
 // [Helmet](https://github.com/helmetjs/helmet) helps you secure your
 // Express apps by setting various HTTP headers.
 // Install the package, then require it.
-const helmet = require('helmet');
-app.use(helmet());
 
 
 /** 2) Hide potentially dangerous information - `helmet.hidePoweredBy()` */
